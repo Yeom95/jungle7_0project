4d7487d7d875cfdafe51511e93008e0d50997655
yeom0325 <120087777+yeom0325@users.noreply.github.com>
2024-07-02 20:54:26 +0900

    Merge branch 'main' of https://github.com/Yeom95/jungle7_0project

--- 7월 Week0 프로젝트/app.py
+++ 7월 Week0 프로젝트/app.py
@@ -1,8 +1,8 @@
 import calendar
 from datetime import datetime
-import json
 from flask import Flask,render_template,jsonify,request
 from flask.json.provider import JSONProvider
+from flask_jwt_extended import *
 from bson import ObjectId
 app = Flask(__name__)
 
@@ -10,30 +10,27 @@ from pymongo import MongoClient
 #client = MongoClient('mongodb://test:test@localhost',27017)
 client = MongoClient('localhost',27017)
 db = client.dbjungle
-collection = db['moneyPlan']
 
-class CustomJSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
+#토큰 생성에 사용될 key를 flask 환경 변수에 등록
+app.config.update(
+    DEBUG = True,
+    JWT_SECTRET_KEY = "Secret Key"
+)
 
-
-class CustomJSONProvider(JSONProvider):
-    def dumps(self, obj, **kwargs):
-        return json.dumps(obj, **kwargs, cls=CustomJSONEncoder)
-
-    def loads(self, s, **kwargs):
-        return json.loads(s, **kwargs)
-
-
-app.json = CustomJSONProvider(app)
-
+#JWT 확장 모듈을 flask 어플리케이션에 등록
+jwt = JWTManager(app)
 
 #유저별 주간,월간 금액합계 문제
 #날짜,요일 계산
 #수정 기능 사용 시 폼처리
 #유저 데이터 형식
+
+#연도와 몇월인지 받으면,날짜와 해당 달 이름 가져오는 함수
+def get_month_days(year,month):
+        cal = calendar.Calendar(firstweekday=6)
+        month_days = cal.monthdayscalendar(year,month)
+        month_name = calendar.month_name[month]
+        return month_name,month_days
 
 @app.route('/')
 def home():
@@ -41,7 +38,6 @@ def home():
 
 @app.route('/login',methods=['GET'])
 def login():
-    #로그인 기능 구현
     return 0
 
 @app.route('/logout',methods=['GET'])
@@ -56,70 +52,36 @@ def register():
 
 @app.route('/getAllRank',methods=['GET'])
 def getAllRank():
-    # MongoDB 집계 파이프라인
-    pipeline = [
-        {"$group": {"_id": "$userId", "total_cost": {"$sum": {"$toInt": "$cost"}}}},
-        {"$sort": {"total_cost": 1}}  # total_cost를 오름차순으로 정렬
-    ]
-    # 집계 실행
-    result = list(collection.aggregate(pipeline))
-
-    # 결과를 JSON 형식으로 반환?
-    return jsonify({'result': 'success', 'moneyRankList': result})
+    #유저별 금액합계 조회 및 정렬 기능 구현
+    return 0
 
 @app.route('/setMyCost')
 def myCalendar():
     today = datetime.today()
 
     year = today.year
-    month = today.month + 1
+    month = today.month
 
-    cal = calendar.Calendar(firstweekday=6)
-    month_days = cal.monthdayscalendar(year,month)
-
-    month_name = calendar.month_name[month]
-    print(month_name)
+    month_name,month_days = get_month_days(year,month)
 
     return render_template('MoneyRankEditMode.html',month_name=month_name, year=year, month_days=month_days)
-    
 
+@app.route('/get_calendar',methods = ['GET'])
+def get_calendar():
+    year = int(request.args.get('year'))
+    month = int(request.args.get('month'))
+    month_name,month_days = get_month_days(year,month)
 
+    return jsonify({
+        'month_name' : month_name,
+        'month_days' : month_days,
+        'year' : year
+    })
 
-# 데이터 삽입을 처리하는 API 엔드포인트
-@app.route('/addCost', methods=['POST'])
+@app.route('/addCost',methods=['POST'])
 def addCost():
-    try:
-        # 클라이언트로부터 JSON 데이터 받기
-        userId_receive = request.form['userId_give']
-        userName_receive = request.form['userName_give']
-        date_receive = request.form['Date_give']
-        category_receive = request.form['Category_give']
-        cost_receive = request.form['Cost_give']
-
-        # 데이터 유효성 검사 (Id/Name/Date는 사용자가 입력하는게 아니기에 굳이 안해도되는가?)
-        if not (userId_receive and userName_receive and date_receive and category_receive and cost_receive):
-            return jsonify({"error": "필수 필드가 누락되었습니다."}), 400
-
-        # MongoDB에 데이터 삽입
-        data = {
-            'userId': userId_receive,
-            'userName': userName_receive,
-            'date': date_receive,
-            'category': category_receive,
-            'cost': cost_receive
-        }
-
-        result = collection.insert_one(data)
-
-        # 삽입 결과 응답
-        response = {
-            "message": "데이터가 성공적으로 삽입되었습니다.",
-            "inserted_id": str(result.inserted_id)
-        }
-        return jsonify(response), 200
-
-    except Exception as e:
-        return jsonify({"error": str(e)}), 500
+    #사용금액 등록 기능 구현
+    return 0
 
 @app.route('/editCost',methods=['POST'])
 def editCost():
